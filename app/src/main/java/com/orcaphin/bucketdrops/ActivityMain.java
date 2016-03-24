0e741c7866c771351cf11dcf190373d88b820d9d
OrcaphinRep <orcaphin16@gmail.com>
2016-03-24 14:28:56 +0530

    added glide to load background image

--- app/src/main/java/com/orcaphin/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/orcaphin/bucketdrops/ActivityMain.java
@@ -2,14 +2,28 @@ package com.orcaphin.bucketdrops;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.support.v7.widget.Toolbar;
+
+
+import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity
 {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        initBackgroundImage();
+    }
+
+    private void initBackgroundImage()
+    {
+        ImageView background= (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this).load(R.drawable.background).centerCrop().into(background);
     }
 }
