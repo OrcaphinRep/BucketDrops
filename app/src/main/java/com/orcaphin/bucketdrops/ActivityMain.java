770b01954cb076eacb52ae127db6be80c315297e
OrcaphinRep <orcaphin16@gmail.com>
2016-03-24 19:07:09 +0530

    dialog fragment created completed. Added DatePicker, Image Button , close button and modification to main activity to launch the dialog

--- app/src/main/java/com/orcaphin/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/orcaphin/bucketdrops/ActivityMain.java
@@ -2,6 +2,8 @@ package com.orcaphin.bucketdrops;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.support.v7.widget.Toolbar;
 
@@ -9,16 +11,31 @@ import android.support.v7.widget.Toolbar;
 import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity
-{
-    Toolbar toolbar;
+{   Button  mBtnAdd;
+    Toolbar mtoolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar= (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mBtnAdd= (Button) findViewById(R.id.btn_add);
+        mtoolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mtoolbar);
         initBackgroundImage();
+        mBtnAdd.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                showDialogAdd();
+            }
+        });
+    }
+
+    private void showDialogAdd()
+    {
+        DialogAdd dialog=new DialogAdd();
+        dialog.show(getSupportFragmentManager(),"Bucket Drops Add");
     }
 
     private void initBackgroundImage()
